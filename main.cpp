b139dc8260de2cb8b712f2ce2182a37eb9dc77ac
Pavel A Taranov <itarpgm@gmail.com>
2021-12-13 16:56:03 +0300

    Optimization of map display and initialization

--- main.cpp
+++ main.cpp
@@ -7,39 +7,8 @@
 #include <stdio.h>
 #include <windows.h>
 
-// Карта игрового поля имеет фиксированный размер, инициализируется бордюром - '#' и свободным полем - символом пробел ' '
-char caMap[30][120+1] = {
-  "########################################################################################################################",
-  "#                                                                                                                      #",
-  "#                                                                                                                      #",
-  "#                                                                                                                      #",
-  "#                                                                                                                      #",
-  "#                                                                                                                      #",
-  "#                                                                                                                      #",
-  "#                                                                                                                      #",
-  "#                                                                                                                      #",
-  "#                                                                                                                      #",
-  "#                                                                                                                      #",
-  "#                                                                                                                      #",
-  "#                                                                                                                      #",
-  "#                                                                                                                      #",
-  "#                                                                                                                      #",
-  "#                                                                                                                      #",
-  "#                                                                                                                      #",
-  "#                                                                                                                      #",
-  "#                                                                                                                      #",
-  "#                                                                                                                      #",
-  "#                                                                                                                      #",
-  "#                                                                                                                      #",
-  "#                                                                                                                      #",
-  "#                                                                                                                      #",
-  "#                                                                                                                      #",
-  "#                                                                                                                      #",
-  "#                                                                                                                      #",
-  "#                                                                                                                      #",
-  "#                                                                                                                      #",
-  "########################################################################################################################"
-};
+// Карта игрового поля имеет фиксированный размер
+char caMap[30][120+1];
 
 int main(int argc, char *argv[]) {
   //Получить дескриптор стандартного вывода
@@ -58,37 +27,19 @@ int main(int argc, char *argv[]) {
   cursorPos.Y = 0;
   SetConsoleCursorPosition(handle, cursorPos);
 
+  //Карта инициализируется бордюром - '#' и свободным полем для размещения объектов - символом пробел ' '
+  for(int posY = 0; posY < 30; posY++) {
+    for(int posX = 0; posX < 120; posX++)
+      if (posY == 0 || posY == 29 || posX == 0 || posX == 119)
+	caMap[posY][posX] = '#';
+      else
+	caMap[posY][posX] = ' ';
+    caMap[posY][120] = '\0';
+  }
+
   //Вывести карту игрового поля на экран
-  printf("%s\n",caMap[0]);
-  printf("%s\n",caMap[1]);
-  printf("%s\n",caMap[2]);
-  printf("%s\n",caMap[3]);
-  printf("%s\n",caMap[4]);
-  printf("%s\n",caMap[5]);
-  printf("%s\n",caMap[6]);
-  printf("%s\n",caMap[7]);
-  printf("%s\n",caMap[8]);
-  printf("%s\n",caMap[9]);
-  printf("%s\n",caMap[10]);
-  printf("%s\n",caMap[11]);
-  printf("%s\n",caMap[12]);
-  printf("%s\n",caMap[13]);
-  printf("%s\n",caMap[14]);
-  printf("%s\n",caMap[15]);
-  printf("%s\n",caMap[16]);
-  printf("%s\n",caMap[17]);
-  printf("%s\n",caMap[18]);
-  printf("%s\n",caMap[19]);
-  printf("%s\n",caMap[20]);
-  printf("%s\n",caMap[21]);
-  printf("%s\n",caMap[22]);
-  printf("%s\n",caMap[23]);
-  printf("%s\n",caMap[24]);
-  printf("%s\n",caMap[25]);
-  printf("%s\n",caMap[26]);
-  printf("%s\n",caMap[27]);
-  printf("%s\n",caMap[28]);
-  printf("%s\n",caMap[29]);
+    for(char *str : caMap)
+      printf("%s\n", str);
 
   //Перед выходом в консоль вернуть курсору начальные настройки
   SetConsoleCursorInfo(handle, &structCursorInfoTemp);
